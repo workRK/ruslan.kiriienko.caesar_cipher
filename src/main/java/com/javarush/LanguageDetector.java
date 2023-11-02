fd5af010d955b89fba8c5113868a2d117c45d00a
workRK <rk29work@proton.me>
2023-11-02 14:06:31 +0200

    add class LanguageDetector

---
+++ src/main/java/com/javarush/LanguageDetector.java
@@ -0,0 +1,22 @@
+package com.javarush;
+
+public class LanguageDetector {
+
+    public boolean containsLatin(String text) {
+        for (char c : text.toCharArray()) {
+            if (Character.UnicodeScript.of(c) == Character.UnicodeScript.LATIN) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsCyrillic(String text) {
+        for (char c : text.toCharArray()) {
+            if (Character.UnicodeScript.of(c) == Character.UnicodeScript.CYRILLIC) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/com/javarush/Main.java
+++
@@ -1,8 +0,0 @@
-package com.javarush;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Dev");
-    }
-}
