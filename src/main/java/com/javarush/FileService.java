631006e7118a1e4f53994cfc46a772f627f98281
workRK <rk29work@proton.me>
2023-11-02 15:05:29 +0200

    add class FileService

---
+++ src/main/java/com/javarush/FileService.java
@@ -0,0 +1,36 @@
+package com.javarush;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileService {
+    private String filePath;
+
+    public FileService(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String readFile() {
+        StringBuilder content = new StringBuilder();
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line).append(System.lineSeparator());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    public void writeFile(String writeFilePath, String content) {
+        try (FileWriter fileWriter = new FileWriter(filePath);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
