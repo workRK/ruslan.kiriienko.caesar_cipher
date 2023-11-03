ddb992766ceea9ad241d3412a10ac16e9760558b
workRK <rk29work@proton.me>
2023-11-03 12:57:38 +0200

    add class BruteForceDecrypt

---
+++ src/main/java/com/javarush/BruteForceDecrypt.java
@@ -0,0 +1,29 @@
+package com.javarush;
+
+public class BruteForceDecrypt {
+    private String content;
+    private String alphabet;
+
+    public BruteForceDecrypt(String content) {
+        this.content = content;
+    }
+
+    public void bruteForceDecrypt() {
+        LanguageDetector languageDetector = new LanguageDetector();
+
+        if (languageDetector.containsLatin(content)) {
+            alphabet = "abcdefghijklmnopqrstuvwxyz";
+        } else if (languageDetector.containsCyrillic(content)) {
+            alphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+        } else {
+            System.out.println("Мова не підтримується.");
+        }
+
+        for (int key = 0; key < alphabet.length(); key++) {
+            DecryptorCaesar decryptorCaesar = new DecryptorCaesar(key, content);
+            String decryptedText = decryptorCaesar.decryptCaesar();
+
+            System.out.println("Ключ: " + key + " - розшифрований текст: " + decryptedText);
+        }
+    }
+}
--- src/main/java/com/javarush/CaesarCipher.java
+++ src/main/java/com/javarush/CaesarCipher.java
@@ -2,6 +2,12 @@ package com.javarush;
 
 public class CaesarCipher {
     public static void main(String[] args) {
-
+        String filePath = "C:\\Users\\Raketa\\IdeaProjects\\ruslan.kiriienko.caesar_cipher\\src\\main\\java\\com\\javarush\\text.txt";
+        FileService fileService = new FileService(filePath);
+        String contentEncrypt = fileService.readFile();
+        EncryptorCaesar encryptorCaesar = new EncryptorCaesar(7, contentEncrypt);
+        String content = encryptorCaesar.encryptCaesar();
+        BruteForceDecrypt bruteForceDecrypt = new BruteForceDecrypt(content);
+        bruteForceDecrypt.bruteForceDecrypt();
     }
 }
