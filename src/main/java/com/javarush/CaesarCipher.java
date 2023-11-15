7ce9cbf20eca0c0ab300ce4988b5151488771898
workRK <rk29work@proton.me>
2023-11-15 20:59:32 +0200

    Modification CaesarCipher

--- src/main/java/com/javarush/CaesarCipher.java
+++ src/main/java/com/javarush/CaesarCipher.java
@@ -27,13 +27,46 @@ public class CaesarCipher {
             String writeFilePath = filePath.substring(0, lastDotindex) + "[" + command + "]" + filePath.substring(lastDotindex);
             fileService.writeFile(writeFilePath, decryptedContent);
         } else if (args.length == 2 && args[0].equals("BRUTE_FORCE")) {
-            String command = args[0];
             String filePath = args[1];
 
             FileService fileService = new FileService();
             String content = fileService.readFile(filePath);
             BruteForceDecrypt bruteForceDecrypt = new BruteForceDecrypt(content);
             bruteForceDecrypt.bruteForceDecrypt();
+        } else if (args.length == 0) {
+            CLI cli = new CLI();
+            String command = cli.promptForCommand();
+            if (command.equals("ENCRYPT")) {
+                String filePath = cli.promptForFilePath();
+                int key = cli.promptForKey();
+                cli.close();
+                FileService fileService = new FileService();
+                String content = fileService.readFile(filePath);
+                EncryptorCaesar encryptorCaesar = new EncryptorCaesar(content, key);
+                String encryptedContent = encryptorCaesar.encryptCaesar();
+                int lastDotindex = filePath.lastIndexOf(".");
+                String writeFilePath = filePath.substring(0, lastDotindex) + "[" + command + "]" + filePath.substring(lastDotindex);
+                fileService.writeFile(writeFilePath, encryptedContent);
+            } else if (command.equals("DECRYPT")) {
+                String filePath = cli.promptForFilePath();
+                int key = cli.promptForKey();
+                cli.close();
+                FileService fileService = new FileService();
+                String content = fileService.readFile(filePath);
+                DecryptorCaesar decryptorCaesar = new DecryptorCaesar(content, key);
+                String decryptedContent = decryptorCaesar.decryptCaesar();
+                int lastDotindex = filePath.lastIndexOf(".");
+                String writeFilePath = filePath.substring(0, lastDotindex) + "[" + command + "]" + filePath.substring(lastDotindex);
+                fileService.writeFile(writeFilePath, decryptedContent);
+            } else if (command.equals("BRUTE_FORCE")) {
+                String filePath = cli.promptForFilePath();
+                cli.close();
+                FileService fileService = new FileService();
+                String content = fileService.readFile(filePath);
+                BruteForceDecrypt bruteForceDecrypt = new BruteForceDecrypt(content);
+                bruteForceDecrypt.bruteForceDecrypt();
+            }
+
         }
     }
 }
