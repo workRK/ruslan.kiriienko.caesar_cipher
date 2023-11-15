b0467cd2f8ba7ddb5df225e567585f44d9c59e47
workRK <rk29work@proton.me>
2023-11-15 16:27:22 +0200

    Add README.md

--- src/main/java/com/javarush/CaesarCipher.java
+++ src/main/java/com/javarush/CaesarCipher.java
@@ -2,5 +2,18 @@ package com.javarush;
 
 public class CaesarCipher {
     public static void main(String[] args) {
+
+        if (args.length == 3 && args[0].equals("ENCRYPT")) {
+            String command = args[0];
+            String filePath = args[1];
+            int key = Integer.parseInt(args[2]);
+
+            FileService fileService = new FileService();
+            String content = fileService.readFile(filePath);
+            EncryptorCaesar encryptorCaesar = new EncryptorCaesar(content, key);
+            String encryptedContent = encryptorCaesar.encryptCaesar();
+
+            fileService.writeFile(filePath, encryptedContent);
+        }
     }
 }
